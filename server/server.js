8abfbcbace368f862c7b40be97c7b72befa296ec
Jiovanni Rosario <jiovanniro@users.noreply.github.com>
2017-12-05 14:01:15 -0600

    Updated comments

--- server/server.js
+++ server/server.js
@@ -69,11 +69,6 @@ io.on('connection', (socket) => {
                 user.name, 
                 message.text               
             ));
-            
-            // io.emit('newMessage', generateMessage(
-            //     message.from, 
-            //     message.text
-            // ));
         }
         callback(); //will call the callback function in the client
     });
@@ -86,8 +81,6 @@ io.on('connection', (socket) => {
             io.to(user.room).emit('updateUserList', users.getUserList(user.room));       
             io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left`));     
         }
-
-
     });
 });
 
